b590f44447014ec4e49bf89b71c1e0af24b1d084
Aiman <aimandanish5308@gmail.com>
2024-12-17 09:44:34 +0000

    initial attempt

--- +D6/Guard.m
+++ +D6/Guard.m
@@ -1,8 +1,10 @@
 classdef Guard < handle
     properties (SetAccess = private)
         position (1,2) double
+        initialPosition (1,2) double
         direction (1,2) double = [-1, 0] % up by default
         isInBounds (1,1) logical = true
+        isInLoop (1,1) logical = false
         stepsTaken (1,1) double = 0
     end
 
@@ -16,11 +18,14 @@ classdef Guard < handle
                 position = nan(1,2)
             end
             obj.position = position;
+            obj.initialPosition = position;
         end
+
 
         function nextPosition = get.nextPosition(self)
             nextPosition = self.position + self.direction;
         end
+
 
         function rotate90DegClockwise(self)
             rotationMatrix = [0, 1; -1, 0];
@@ -29,12 +34,14 @@ classdef Guard < handle
             self.direction = newDirection';
         end
 
+
         function step(self)
             self.position = self.nextPosition;
             self.stepsTaken = self.stepsTaken + 1;
         end
 
-        function updateInBoundsStatus(self,mapGridSize)
+
+        function checkIfInBounds(self,mapGridSize)
             arguments
                 self (1,1) D6.Guard
                 mapGridSize (1,2) double
@@ -43,6 +50,18 @@ classdef Guard < handle
             col = self.position(2);
             if row < 1 || row > mapGridSize(1) || col < 1 || col > mapGridSize(2)
                 self.isInBounds = false;
+            end
+        end
+
+
+        function checkIfInLoop(self,previousDirections)
+            arguments
+                self (1,1) D6.Guard
+                previousDirections (1,1) cell % cell of cell(s) of numeric array
+            end
+            currentDirection = self.direction;
+            if any(cellfun(@(x) all(x == currentDirection),previousDirections{:}))
+                self.isInLoop = true;
             end
         end
     end
--- D6.m
+++ D6.m
@@ -4,14 +4,48 @@ import D6.*
 filename = "D6 Data.txt";
 data = readlines(filename);
 
-map = Map(data);
+map = PatrolMap("data",data);
 
 tic
-while map.guard.isInBounds
+while map.guard.isInBounds && ~map.guard.isInLoop
     map.step();
 end
 toc
-map.exportGrid();
+map.exportGrid('bUseDirectionSymbols',true);
 
 nDistinctPositions = sum(map.pathWalked,"all");
-fprintf("Number of distinct positions: %i\n", nDistinctPositions)
+fprintf("Number of distinct positions: %i\n\n", nDistinctPositions)
+
+%% Part 2
+originalGrid = map.grid;
+gridSize = map.gridSize;
+nGridPositions = numel(originalGrid);
+initialPosition = map.guard.initialPosition;
+
+tic
+nWorkers = 6;
+positionCausesLoop = false(size(originalGrid));
+parfor (iPosition = 1:nGridPositions,nWorkers)
+    if originalGrid(iPosition) == "#"
+        continue
+    end
+
+    if originalGrid(iPosition) == "^"
+        continue
+    end
+
+    modifiedGrid = originalGrid;
+    modifiedGrid(iPosition) = "#";
+    map = PatrolMap("grid",modifiedGrid);
+
+    while map.guard.isInBounds && ~map.guard.isInLoop
+        map.step();
+        if map.guard.isInLoop
+            positionCausesLoop(iPosition) = true;
+        end
+    end
+end
+toc
+
+nPositionsThatCauseLoop = sum(positionCausesLoop,"all");
+fprintf("Number of positions that cause loop: ", nPositionsThatCauseLoop)
