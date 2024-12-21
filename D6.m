fa536b559484a2a674b75a6a1c935f71d1e5046d
Aiman <aimandanish5308@gmail.com>
2024-12-21 18:04:42 +0000

    D6 optimisation

--- D6.m
+++ D6.m
@@ -19,38 +19,41 @@ fprintf("Number of distinct positions: %i\n\n", nDistinctPositions)
 %% Part 2
 originalGrid = map.grid;
 gridSize = map.gridSize;
-nGridPositions = numel(originalGrid);
-initialPosition = map.guard.initialPosition;
 
-% 6 workers: ~138s
-% 12 workers: ~119s
+% only place new obstacles in places where th guard will actually reach
+gridPositionsToCheck = find(map.pathWalked);
+
+% don't place new obstacles on initial position
+initialPosition = map.guard.initialPosition;
+initialPosition = sub2ind(map.gridSize,initialPosition(1),initialPosition(2));
+gridPositionsToCheck = gridPositionsToCheck(gridPositionsToCheck ~= initialPosition);
+
+
+% 6 workers: ~26s
+% 12 workers: ~23s
 nWorkers = 6;
-parpool(nWorkers);
+if isempty(gcp("nocreate"))
+    parpool(nWorkers);
+end
 
 tic
-positionCausesLoop = false(size(originalGrid));
-parfor (iPosition = 1:nGridPositions,nWorkers)
-    if originalGrid(iPosition) == "#"
-        continue
-    end
-
-    if originalGrid(iPosition) == "^"
-        continue
-    end
-
-    modifiedGrid = originalGrid; %#ok<PFBNS>
-    modifiedGrid(iPosition) = "#";
+nGridPositionsToCheck = numel(gridPositionsToCheck);
+bPositionCausesLoop = false(size(gridPositionsToCheck));
+parfor (iPosition = 1:nGridPositionsToCheck,nWorkers)
+    obstaclePosition = gridPositionsToCheck(iPosition);
+    modifiedGrid = originalGrid; 
+    modifiedGrid(obstaclePosition) = "#";
     map = PatrolMap("grid",modifiedGrid);
 
     while map.guard.isInBounds && ~map.guard.isInLoop
         map.step();
         if map.guard.isInLoop
-            positionCausesLoop(iPosition) = true;
+            bPositionCausesLoop(iPosition) = true;
         end
     end
 end
 toc
 delete(gcp('nocreate'))
 
-nPositionsThatCauseLoop = sum(positionCausesLoop,"all");
+nPositionsThatCauseLoop = sum(bPositionCausesLoop,"all");
 fprintf("Number of positions that cause loop: %i\n", nPositionsThatCauseLoop)
