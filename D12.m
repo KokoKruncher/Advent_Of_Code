f61a5d3119b0b920cdc28aec68fddc2559dab5fd
Aiman <aimandanish5308@gmail.com>
2025-01-19 13:28:38 +0000

    algorithm to count number of sides

--- D12.m
+++ D12.m
@@ -38,26 +38,94 @@ fprintf("Total fence price: %i\n",totalFencePrice)
 % get vertex locations for each region and make a graph. side nodes are nodes that have
 % degree of exactly 2. graph can have up to 2 components, 1 for outer fence and one for
 % inner fence if it exists. DFS each component, add a side when direction changes.
-
+nSides = nan(nRegions,1);
 for iRegion = 1:1
     isNodeInRegion = nodeRegionIndx == iRegion;
+
     regionNodes = gardenGraph.Nodes(isNodeInRegion,:);
     [vertexRows,vertexCols] = gridIndxToVertex(regionNodes.iRow,regionNodes.iCol);
     vertexGraph = createVertexGraph(vertexRows,vertexCols);
     
-    % vertices on the side (outside or inside if it exists) have degree less than 4
-    isSideVertex = vertexGraph.degree < 4;
-    nVertices = vertexGraph.numnodes;
-    allVertices = 1:nVertices;
-    sideGraph = vertexGraph.rmnode(allVertices(~isSideVertex));
+    % vertices not on the side (outside or inside if it exists) have degree equal to 8
+    isInsideRegionArea = vertexGraph.degree == 8;
+    regionSidesGraph = vertexGraph.rmnode(find(isInsideRegionArea));
+
+    % diagonal edges have weight 0.5
+    isDiagonalEdge = regionSidesGraph.Edges.Weights == 0.5;
+    regionSidesGraph = regionSidesGraph.rmedge(find(isDiagonalEdge));
+
+    % the last straggelers of internal edges are those connecting "blocks" that are poking
+    % out of the main shape by 1 block (in a 1 bcock cycle), making nodes that are of
+    % degree 3
+    regionSidesGraph = removeLastInternalEdges(regionSidesGraph);
+
+    allCycles = regionSidesGraph.allcycles();
+    nCycles = numel(allCycles);
+    if nCycles == 2
+        disp("Found 2 cycles")
+    elseif nCycles == 0 || nCycles > 2
+        error("Watafak, found %i cycles",nCycles)
+    end
+    
+    nDirectionChanges = nan(nCycles,1);
+    for iCycle = 1:nCycles
+        thisCycle = allCycles{iCycle};
+        coords = [regionSidesGraph.Nodes.iRow(thisCycle), ...
+            regionSidesGraph.Nodes.iCol(thisCycle)];
+        directions = diff(coords,1,1);
+        differenceToPreviousDirection = diff(directions,1,1);
+        isDirectionChange = any(differenceToPreviousDirection ~= 0, 2);
+        nDirectionChanges(iCycle) = nnz(isDirectionChange);
+    end
+    nSides(iRegion) = sum(nDirectionChanges);
 end
+
 figure
 x = vertexGraph.Nodes.iCol;
 y = vertexGraph.Nodes.iRow;
 z = zeros(size(x));
 plot(vertexGraph,'XData',x,'YData',y)
+
+figure
+x = regionSidesGraph.Nodes.iCol;
+y = regionSidesGraph.Nodes.iRow;
+z = zeros(size(x));
+plot(regionSidesGraph,'XData',x,'YData',y)
 %% Functions
 function gardenMap = parseInput(input)
 gardenMap = split(input,"");
 gardenMap = gardenMap(:,2:end-1);
+end
+
+
+
+function regionSidesGraph = removeLastInternalEdges(regionSidesGraph)
+[~,allCycleEdges] = regionSidesGraph.allcycles();
+isOneBlockCycle = cellfun(@numel,allCycleEdges) == 4;
+
+if ~any(isOneBlockCycle)
+    return
+end
+
+oneBlockCycleEdges = allCycleEdges(isOneBlockCycle);
+oneBlockCycleEdges = [oneBlockCycleEdges{:}]; % concat into one row vector
+
+nNodes = regionSidesGraph.numnodes;
+sourceNodes = regionSidesGraph.Edges.EndNodes(:,1);
+targetNodes = regionSidesGraph.Edges.EndNodes(:,2);
+nodeDegrees = regionSidesGraph.degree();
+degreeOfNodeId = dictionary(1:nNodes,nodeDegrees');
+sourceNodeDegrees = degreeOfNodeId(sourceNodes);
+targetNodeDegrees = degreeOfNodeId(targetNodes);
+isEdgeToNodesOfDegree3 = targetNodeDegrees == 3 & sourceNodeDegrees == 3;
+iEdgeToNodesOfDegree3 = find(isEdgeToNodesOfDegree3);
+
+isInternalEdge = ismember(oneBlockCycleEdges,iEdgeToNodesOfDegree3);
+
+if ~any(isInternalEdge)
+    return
+end
+
+internalEdges = oneBlockCycleEdges(isInternalEdge);
+regionSidesGraph = regionSidesGraph.rmedge(internalEdges);
 end
--- createVertexGraph.m
+++ createVertexGraph.m
@@ -21,42 +21,78 @@ nodeTable.iRow = nodeRows(:);
 nodeTable.iCol = nodeCols(:);
 
 edgeTable = table();
-edgeTable.EndNodes = nan(2*nPoints,2);
+edgeTable.EndNodes = nan(8*nPoints,2);
+edgeTable.Weights = nan(8*nPoints,1);
 mapWidth = width(map);
 mapHeight = height(map);
 iNode = 0;
 iEdge = 0;
 for iWidth = 1:mapWidth
     for iHeight = 1:mapHeight
-        % need to search all 4 directions
+        % need to search all 4 directions plus diagonals to give info on whether it is on
+        % the edges of the region or not
         point = map(iHeight,iWidth);
         iNode = iNode + 1;
         if ~point
             continue
         end
-
+        
+        % check up, down, left, right, edge weight 1
         if iHeight > 1 && map(iHeight-1,iWidth)
             iNodeUp = iNode - 1;
             iEdge = iEdge + 1;
-            edgeTable{iEdge,:} = [iNode, iNodeUp];
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeUp];
+            edgeTable.Weights(iEdge) = 1;
         end
 
         if iHeight < mapHeight && map(iHeight+1,iWidth)
             iNodeDown = iNode + 1;
             iEdge = iEdge + 1;
-            edgeTable{iEdge,:} = [iNode, iNodeDown];
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeDown];
+            edgeTable.Weights(iEdge) = 1;
         end
 
         if iWidth < mapWidth && map(iHeight,iWidth+1)
             iNodeRight = sub2ind([mapHeight,mapWidth],iHeight,iWidth+1);
             iEdge = iEdge + 1;
-            edgeTable{iEdge,:} = [iNode, iNodeRight];
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeRight];
+            edgeTable.Weights(iEdge) = 1;
         end
 
         if iWidth > 1 && map(iHeight,iWidth-1)
             iNodeLeft = sub2ind([mapHeight,mapWidth],iHeight,iWidth-1);
             iEdge = iEdge + 1;
-            edgeTable{iEdge,:} = [iNode, iNodeLeft];
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeLeft];
+            edgeTable.Weights(iEdge) = 1;
+        end
+
+        % check diagonals, edge weight 0.5
+        if iHeight > 1 && iWidth > 1 && map(iHeight-1,iWidth-1)
+            iNodeTopLeft = sub2ind([mapHeight,mapWidth],iHeight-1,iWidth-1);
+            iEdge = iEdge + 1;
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeTopLeft];
+            edgeTable.Weights(iEdge) = 0.5;
+        end
+
+        if iHeight > 1 && iWidth < mapWidth && map(iHeight-1,iWidth+1)
+            iNodeTopRight = sub2ind([mapHeight,mapWidth],iHeight-1,iWidth+1);
+            iEdge = iEdge + 1;
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeTopRight];
+            edgeTable.Weights(iEdge) = 0.5;
+        end
+
+        if iHeight < mapHeight && iWidth > 1 && map(iHeight+1,iWidth-1)
+            iNodeBottomLeft = sub2ind([mapHeight,mapWidth],iHeight+1,iWidth-1);
+            iEdge = iEdge + 1;
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeBottomLeft];
+            edgeTable.Weights(iEdge) = 0.5;
+        end
+
+        if iHeight < mapHeight && iWidth < mapWidth && map(iHeight+1,iWidth+1)
+            iNodeBottomRight = sub2ind([mapHeight,mapWidth],iHeight+1,iWidth+1);
+            iEdge = iEdge + 1;
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeBottomRight];
+            edgeTable.Weights(iEdge) = 0.5;
         end
     end
 end
