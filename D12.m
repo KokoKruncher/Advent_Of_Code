5cb38fac87da095372e84e8000695c292f41e848
Aiman <aimandanish5308@gmail.com>
2025-01-25 02:15:33 +0000

    tried simplistic corner node identification. corner node definition doesn't cover all the cases

--- D12.m
+++ D12.m
@@ -2,17 +2,17 @@ clear; clc; close all
 
 %% Part 1
 filename = "D12 Data.txt";
-input = readlines(filename);
-% input = ["RRRRIICCFF"; ...
-% "RRRRIICCCF"; ...
-% "VVRRRCCFFF"; ...
-% "VVRCCCJFFF"; ...
-% "VVVVCJJCFE"; ...
-% "VVIVCCJJEE"; ...
-% "VVIIICJJEE"; ...
-% "MIIIIIJJEE"; ...
-% "MIIISIJEEE"; ...
-% "MMMISSJEEE"];
+% input = readlines(filename);
+input = ["RRRRIICCFF"; ...
+         "RRRRIICCCF"; ...
+         "VVRRRCCFFF"; ...
+         "VVRCCCJFFF"; ...
+         "VVVVCJJCFE"; ...
+         "VVIVCCJJEE"; ...
+         "VVIIICJJEE"; ...
+         "MIIIIIJJEE"; ...
+         "MIIISIJEEE"; ...
+         "MMMISSJEEE"];
 
 gardenMap = parseInput(input);
 
@@ -35,91 +35,130 @@ totalFencePrice = sum(fencePrices);
 fprintf("Total fence price: %i\n",totalFencePrice)
 
 %% Part 2
-nSidesInRegion = nan(nRegions,1);
+% Number of sides = number of corner nodes. Corner nores are nodes with degree 2 and with
+% an edge weight sum of 3. This eliminates nodes of degree 2 where it's neighbours form a
+% straight line with it.
+isNodeDegree2 = gardenGraph.degree() == 2;
+nodesDegree2 = find(isNodeDegree2);
+isEdgeOfNodeDegree2 = any(ismember(gardenGraph.Edges.EndNodes,nodesDegree2),2);
+edgeTableOfNodesDegree2 = gardenGraph.Edges(isEdgeOfNodeDegree2,:);
+
+% Can't vectorise this because some corners are next to other corners, which means some
+% edges need to be considered more than once
+nNodesDegree2 = nnz(isNodeDegree2);
+isCornerNode = false(nNodesDegree2,1);
+for ii = 1:nNodesDegree2
+    thisNode = nodesDegree2(ii);
+    isEdgeOfThisNode = any(edgeTableOfNodesDegree2.EndNodes == thisNode,2);
+    assert(nnz(isEdgeOfThisNode) == 2)
+    edgeWeightSum = sum(edgeTableOfNodesDegree2.Weight(isEdgeOfThisNode));
+    if edgeWeightSum == 3
+        isCornerNode(ii) = true;
+    end
+end
+cornerNodes = nodesDegree2(isCornerNode);
+
+% redefine isCornerNode to reference the entire list of nodes instead of just nodes of
+% degree 2.
+nNodesTotal = height(gardenGraph.Nodes);
+isCornerNode = ismember((1:nNodesTotal)',cornerNodes);
+
+nSidesInRegion = nan(1,nRegions);
 for iRegion = 1:nRegions
     isNodeInRegion = nodeRegionIndx == iRegion;
-    
-    % vertices are where the fence posts are
-    regionNodes = gardenGraph.Nodes(isNodeInRegion,:);
-    vertexGraph = createVertexGraph(regionNodes.iRow,regionNodes.iCol);
-    
-    % debug
-    % 
-    
-    % vertices not on the side (outside or inside if it exists) have degree equal to 8
-    isInsideRegionArea = vertexGraph.degree == 8;
-    regionSidesGraphOld = vertexGraph.rmnode(find(isInsideRegionArea));
-
-    % diagonal edges have weight 0.5
-    isDiagonalEdge = regionSidesGraphOld.Edges.Weights == 0.5;
-    regionSidesGraphOld = regionSidesGraphOld.rmedge(find(isDiagonalEdge));
-
-    % the last stragglers of internal edges are those connecting "blocks" that are poking
-    % out of the main shape by 1 block (in a 1 block cycle), making nodes that are of
-    % degree 3 or sometimes 4
-    regionSidesGraph = removeLastInternalEdges(regionSidesGraphOld);
-
-    allCycles = regionSidesGraph.allcycles();
-    nCycles = numel(allCycles);
-    if nCycles == 2
-        disp("Found 2 cycles")
-    elseif nCycles == 0 || nCycles > 2
-        error("Watafak, found %i cycles",nCycles)
-    end
-
-    if nCycles == 2
-        hAxes = plotRegionAndVertices(regionNodes.iRow,regionNodes.iCol, ...
-            vertexGraph.Nodes.iRow,vertexGraph.Nodes.iCol);
-        hold(hAxes,'on');
-        x = regionSidesGraphOld.Nodes.iCol;
-        y = regionSidesGraphOld.Nodes.iRow;
-        plot(regionSidesGraphOld,'XData',x,'YData',y,'EdgeColor','red')
-        
-        x = regionSidesGraph.Nodes.iCol;
-        y = regionSidesGraph.Nodes.iRow;
-        hPlot = plot(regionSidesGraph,'XData',x,'YData',y,'EdgeColor','green');
-        hold(hAxes,'off')
-    end
-
-    
-    nDirectionChanges = nan(nCycles,1);
-    for iCycle = 1:nCycles
-        thisCycle = allCycles{iCycle};
-        
-        % important to include the final move back to the first node as that can have a
-        % change in direction too
-        nodesInLoop = [thisCycle,thisCycle(1)];
-        coords = [regionSidesGraph.Nodes.iRow(nodesInLoop), ...
-            regionSidesGraph.Nodes.iCol(nodesInLoop)];
-        directions = diff(coords,1,1);
-        differenceToPreviousDirection = diff(directions,1,1);
-        isDirectionChange = any(differenceToPreviousDirection ~= 0, 2);
-        nDirectionChanges(iCycle) = nnz(isDirectionChange) + 1; % add initial direction
-
-       if nCycles == 2
-           highlight(hPlot,thisCycle,circshift(thisCycle,1))
-       end
-    end
-    nSidesInRegion(iRegion) = sum(nDirectionChanges);
-
-    % figure
-    % x = vertexGraph.Nodes.iCol;
-    % y = vertexGraph.Nodes.iRow;
-    % plot(vertexGraph,'XData',x,'YData',y)
-    % 
-    % iLetter = find(nodeRegionIndx == iRegion,1,"first");
-    % letter = gardenGraph.Nodes.letter(iLetter);
-    % figure
-    % x = regionSidesGraph.Nodes.iCol;
-    % y = regionSidesGraph.Nodes.iRow;
-    % plot(regionSidesGraph,'XData',x,'YData',y)
-    % title(letter)
+    nRegionCornerNodes = nnz(isNodeInRegion' & isCornerNode);
+    nSidesInRegion(iRegion) = nRegionCornerNodes;
 end
-
-fencePrices2 = nSidesInRegion(:).*regionAreas(:);
+fencePrices2 = nSidesInRegion.*regionAreas;
 totalFencePrice2 = sum(fencePrices2);
 
 fprintf("Total fence price 2: %i\n",totalFencePrice2)
+
+% nSidesInRegion = nan(nRegions,1);
+% for iRegion = 1:nRegions
+%     isNodeInRegion = nodeRegionIndx == iRegion;
+% 
+%     % vertices are where the fence posts are
+%     regionNodes = gardenGraph.Nodes(isNodeInRegion,:);
+%     vertexGraph = createVertexGraph(regionNodes.iRow,regionNodes.iCol);
+% 
+%     % debug
+%     % 
+% 
+%     % vertices not on the side (outside or inside if it exists) have degree equal to 8
+%     isInsideRegionArea = vertexGraph.degree == 8;
+%     regionSidesGraphOld = vertexGraph.rmnode(find(isInsideRegionArea));
+% 
+%     % diagonal edges have weight 0.5
+%     isDiagonalEdge = regionSidesGraphOld.Edges.Weights == 0.5;
+%     regionSidesGraphOld = regionSidesGraphOld.rmedge(find(isDiagonalEdge));
+% 
+%     % the last stragglers of internal edges are those connecting "blocks" that are poking
+%     % out of the main shape by 1 block (in a 1 block cycle), making nodes that are of
+%     % degree 3 or sometimes 4
+%     regionSidesGraph = removeLastInternalEdges(regionSidesGraphOld);
+% 
+%     allCycles = regionSidesGraph.allcycles();
+%     nCycles = numel(allCycles);
+%     if nCycles == 2
+%         disp("Found 2 cycles")
+%     elseif nCycles == 0 || nCycles > 2
+%         error("Watafak, found %i cycles",nCycles)
+%     end
+% 
+%     if nCycles == 2
+%         hAxes = plotRegionAndVertices(regionNodes.iRow,regionNodes.iCol, ...
+%             vertexGraph.Nodes.iRow,vertexGraph.Nodes.iCol);
+%         hold(hAxes,'on');
+%         x = regionSidesGraphOld.Nodes.iCol;
+%         y = regionSidesGraphOld.Nodes.iRow;
+%         plot(regionSidesGraphOld,'XData',x,'YData',y,'EdgeColor','red')
+% 
+%         x = regionSidesGraph.Nodes.iCol;
+%         y = regionSidesGraph.Nodes.iRow;
+%         hPlot = plot(regionSidesGraph,'XData',x,'YData',y,'EdgeColor','green');
+%         hold(hAxes,'off')
+%     end
+% 
+% 
+%     nDirectionChanges = nan(nCycles,1);
+%     for iCycle = 1:nCycles
+%         thisCycle = allCycles{iCycle};
+% 
+%         % important to include the final move back to the first node as that can have a
+%         % change in direction too
+%         nodesInLoop = [thisCycle,thisCycle(1)];
+%         coords = [regionSidesGraph.Nodes.iRow(nodesInLoop), ...
+%             regionSidesGraph.Nodes.iCol(nodesInLoop)];
+%         directions = diff(coords,1,1);
+%         differenceToPreviousDirection = diff(directions,1,1);
+%         isDirectionChange = any(differenceToPreviousDirection ~= 0, 2);
+%         nDirectionChanges(iCycle) = nnz(isDirectionChange) + 1; % add initial direction
+% 
+%        if nCycles == 2
+%            highlight(hPlot,thisCycle,circshift(thisCycle,1))
+%        end
+%     end
+%     nSidesInRegion(iRegion) = sum(nDirectionChanges);
+% 
+%     % figure
+%     % x = vertexGraph.Nodes.iCol;
+%     % y = vertexGraph.Nodes.iRow;
+%     % plot(vertexGraph,'XData',x,'YData',y)
+%     % 
+%     % iLetter = find(nodeRegionIndx == iRegion,1,"first");
+%     % letter = gardenGraph.Nodes.letter(iLetter);
+%     % figure
+%     % x = regionSidesGraph.Nodes.iCol;
+%     % y = regionSidesGraph.Nodes.iRow;
+%     % plot(regionSidesGraph,'XData',x,'YData',y)
+%     % title(letter)
+% end
+% 
+% fencePrices2 = nSidesInRegion(:).*regionAreas(:);
+% totalFencePrice2 = sum(fencePrices2);
+% 
+% fprintf("Total fence price 2: %i\n",totalFencePrice2)
 
 
 
--- mapToGraph.m
+++ mapToGraph.m
@@ -7,8 +7,11 @@ nodeTable.letter = gardenMap(:);
 nodeTable.iRow = nodeRows(:);
 nodeTable.iCol = nodeCols(:);
 
+% Find edges, assign different weights to each vertical and horizontal directions so that
+% corner nodes can be easily identified
 edgeTable = table();
 edgeTable.EndNodes = nan(2*nPoints,2);
+edgeTable.Weight = nan(2*nPoints,1);
 mapWidth = width(gardenMap);
 mapHeight = height(gardenMap);
 iNode = 0;
@@ -18,17 +21,21 @@ for iWidth = 1:mapWidth
         % only need to search down and right
         letter = gardenMap(iHeight,iWidth);
         iNode = iNode + 1;
-
+        
+        % vertical, weight = 1
         if iHeight < mapHeight && gardenMap(iHeight+1,iWidth) == letter
             iNodeDown = iNode + 1;
             iEdge = iEdge + 1;
-            edgeTable{iEdge,:} = [iNode, iNodeDown];
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeDown];
+            edgeTable.Weight(iEdge) = 1;
         end
 
+        % horizontal, weight = 2
         if iWidth < mapWidth && gardenMap(iHeight,iWidth+1) == letter
             iNodeRight = sub2ind([mapHeight,mapWidth],iHeight,iWidth+1);
             iEdge = iEdge + 1;
-            edgeTable{iEdge,:} = [iNode, iNodeRight];
+            edgeTable.EndNodes(iEdge,:) = [iNode, iNodeRight];
+            edgeTable.Weight(iEdge) = 2;
         end
     end
 end
