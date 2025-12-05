1b0265ea52d65c242ab4d35666dc49874ed4fd71
unknown <aimandanish5308@gmail.com>
2025-12-05 23:24:37 +0000

    D03 Part 2

--- D03.m
+++ D03.m
@@ -2,29 +2,35 @@ clear; clc; close all;
 
 joltages = readlines("D03_Data.txt");
 joltages = joltages(joltages ~= "");
-
-% joltages = ["987654321111111"; ...
-%             "811111111111119"; ...
-%             "234234234234278"; ...
-%             "818181911112111"];
-
 joltages = str2double(num2cell(char(joltages)));
 
 %% Part 1
-maxJoltages = findMaxJoltage(joltages);
-outputJoltage = sum(maxJoltages);
-fprintf("Output voltage = %i\n", outputJoltage);
+maxJoltages1 = findMaxJoltage(joltages, 2);
+outputJoltage1 = sum(maxJoltages1);
+fprintf("Output Joltage, part 1 = %i\n", outputJoltage1);
+
+%% Part 2
+maxJoltages2 = findMaxJoltage(joltages, 12);
+outputJoltage2 = sum(maxJoltages2);
+fprintf("Output Joltage, part 2 = %i\n", outputJoltage2);
 
 %% Functions
-function maxJoltages = findMaxJoltage(joltages)
+function maxJoltages = findMaxJoltage(joltages, nDigits)
+assert(nDigits <= width(joltages), "Number of digits in output joltage cannot exceed number of joltage array columns.");
 nRows = height(joltages);
-[firstDigit, iFirstDigit] = max(joltages(:,1:end-1), [], 2);
+maxJoltages = nan(nRows, nDigits);
 
 remainingDigits = joltages;
-for ii = 1:nRows
-    remainingDigits(ii, 1:iFirstDigit(ii)) = nan;
-end
-secondDigit = max(remainingDigits, [], 2, "omitnan");
+for iDigit = 1:nDigits-1
+    nColumnsToSave = nDigits - (iDigit - 1) - 1;
+    [theseDigits, iTheseDigits] = max(remainingDigits(:,1:end-(nColumnsToSave)), [], 2, "omitnan");
+    maxJoltages(:, iDigit) = theseDigits;
 
-maxJoltages = firstDigit * 10 + secondDigit;
+    for iRow = 1:nRows
+        remainingDigits(iRow, 1:iTheseDigits(iRow)) = nan;
+    end
+end
+maxJoltages(:, end) = max(remainingDigits, [], 2, "omitnan");
+
+maxJoltages = sum(maxJoltages .* 10.^(nDigits-1:-1:0), 2);
 end
