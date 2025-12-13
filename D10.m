79b182858ff9b257ad4dfd348e360bb82bffbf2f
unknown <aimandanish5308@gmail.com>
2025-12-13 01:56:37 +0000

    D10 Part 2, Failed attempt

--- D10.m
+++ D10.m
@@ -5,6 +5,7 @@ manual = manual(manual ~= "");
 
 Machines = parseManual(manual);
 
+% p = createLightPermutations(4)
 %% Part 1
 % Toggling the same button an even number of times does absolutely nothing to the final lights state!
 % Hence, each button should be toggled either 0 times or 1 time only.
@@ -14,22 +15,70 @@ for iMachine = 1:nMachines
     thisMachine = Machines(iMachine);
     theseButtons = thisMachine.buttons;
     nButtons = height(theseButtons);
-    buttonPermutations = createPermutations(nButtons);
+    buttonPermutations = createLightPermutations(nButtons);
     nPermutations = width(buttonPermutations);
     for iPermutation = 1:nPermutations
         thisPermutation = buttonPermutations(:,iPermutation);
         finalState = mod(sum(theseButtons(thisPermutation,:), 1), 2);
         if all(finalState == thisMachine.finalState)
-            Machines(iMachine).minimumToggles = nnz(thisPermutation);
+            Machines(iMachine).minimumLightToggles = nnz(thisPermutation);
             break
         end
     end
 end
 
-minimumTotalToggles = sum([Machines.minimumToggles]);
-fprintf("Minumum total number of toggles = %i\n", minimumTotalToggles)
+minimumLightToggles = sum([Machines.minimumLightToggles]);
+fprintf("Minumum total number of light toggles = %i\n", minimumLightToggles)
+
+%% Part 2
+hPool = parpool("Threads", 4);
+parfor iMachine = 1:nMachines
+    Machines(iMachine).minimumJoltageToggles = solveMinJoltageToggles(Machines(iMachine));
+    
+end
+hPool.delete();
+
+minimumJoltageToggles = sum([Machines.minimumJoltageToggles]);
+fprintf("Minumum total number of joltage toggles = %i\n", minimumJoltageToggles)
+
 
 %% Functions
+function minToggles = solveMinJoltageToggles(Machine)
+N_START_POINTS = 40;
+
+A = Machine.buttons;
+B = Machine.joltages;
+nButtons = height(A);
+minimisationFcn = @(x) sum(x);
+constraintFcnEqualZero = @(x) nonLinearConstraint(x, A, B);
+lowerBounds = zeros(1, nButtons);
+upperBounds = repmat(max(B), 1, nButtons);
+opts = optimoptions("lsqnonlin", "Algorithm", "interior-point");
+
+problemObj = createOptimProblem("lsqnonlin", ...
+    'objective', minimisationFcn, ...
+    'nonlcon', constraintFcnEqualZero, ...
+    'lb', lowerBounds, ...
+    'ub', upperBounds, ...
+    'x0', zeros(1, nButtons), ...
+    'options', opts);
+% x = lsqnonlin(minimisationFcn, zeros(1, nButtons), lowerBounds, upperBounds, [], [], [], [], constraintFcnEqualZero)
+multiStartObj = MultiStart();
+
+warning("off", 'MATLAB:nearlySingularMatrix')
+minToggles = multiStartObj.run(problemObj, N_START_POINTS);
+warning("on", 'MATLAB:nearlySingularMatrix')
+
+minToggles = sum(round(minToggles));
+end
+
+
+function [c, ceq] = nonLinearConstraint(x, A, B)
+c = [];
+ceq = [x * A - B, round(x) - x];
+end
+
+
 function Machines = parseManual(manual)
 Machines = struct();
 nRows = numel(manual);
@@ -58,7 +107,7 @@ end
 end
 
 
-function arr = createPermutations(nButtons)
+function arr = createLightPermutations(nButtons)
 arr = (dec2bin(0:(2^nButtons-1)) == '1').';
 [~, iSort] = sort(sum(arr, 1), "ascend");
 arr = arr(:, iSort);
