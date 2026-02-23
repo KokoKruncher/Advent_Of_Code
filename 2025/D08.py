eb6cd751246cba3dc50cc2e1b2cfe04bf81a64e3
Aiman Danish <aimandanish5308@gmail.com>
2026-02-23 00:02:01 +0000

    2025 D08 Part 2 Python

--- 2025/D08.py
+++ 2025/D08.py
@@ -1,10 +1,60 @@
 import math
+import time
 
 def calculateDistance(position1, position2):
     squareDistance = 0
     for coord1, coord2 in zip(position1, position2):
         squareDistance += (coord1 - coord2) ** 2
     return math.sqrt(squareDistance)
+
+
+class JunctionBoxCollection:
+    def __init__(self, positions):
+        nPositions = len(positions)
+        pairs = [] # each element in the list will be [firstId, secondId, distance]
+        for firstId in range(nPositions):
+            for secondId in range(firstId + 1, nPositions):
+                pairs.append([firstId, secondId, calculateDistance(positions[firstId], positions[secondId])])
+    
+        # Sort by distance (index 2)
+        pairs.sort(key=lambda x: x[2])
+
+        self.positions = positions
+        self.pairs = pairs
+        self.circuitById = list(range(nPositions))
+        self.idsByCircuit = [[x] for x in range(nPositions)]
+        self.mergePairIndex = 0
+
+    def mergeNextCircuitPair(self):
+        # Merge circuits in order
+        nPositions = len(self.positions)
+        thisPair = self.pairs[self.mergePairIndex]
+        self.mergePairIndex += 1
+
+        firstId = thisPair[0]
+        secondId = thisPair[1]
+        firstIdCircuit = self.circuitById[firstId]
+        secondIdCircuit = self.circuitById[secondId]
+        if firstIdCircuit == secondIdCircuit:
+            return
+        for thisId in self.idsByCircuit[secondIdCircuit]:
+            self.circuitById[thisId] = firstIdCircuit
+        self.idsByCircuit[firstIdCircuit] += self.idsByCircuit[secondIdCircuit]
+        self.idsByCircuit[secondIdCircuit] = []
+
+    def calculateCircuitSizes(self):
+        circuitSizes = {}
+        for ii in range(len(self.idsByCircuit)):
+            circuitSizes[ii] = len(self.idsByCircuit[ii])
+        return list(circuitSizes.values())
+    
+    def countCircuits(self):
+        count = 0
+        for ids in self.idsByCircuit:
+            if len(ids) == 0:
+                continue
+            count += 1
+        return count
 
 
 def main():
@@ -15,47 +65,39 @@ def main():
         for line in file:
             positions.append([int(x) for x in line.split(",")])
 
-    nPositions = len(positions)
-    pairDistances = [] # each element in the list will be [firstId, secondId, distance]
-    for firstId in range(nPositions):
-        for secondId in range(firstId + 1, nPositions):
-            pairDistances.append([firstId, secondId, calculateDistance(positions[firstId], positions[secondId])])
-    
-    # Sort by distance (index 2)
-    pairDistances.sort(key=lambda x: x[2])
-
-    # Merge circuits in order
-    circuitById = list(range(nPositions))
-    idsByCircuit = [[x] for x in range(nPositions)]
+    # Part 1
+    tStart = time.time()
+    junctionBoxes = JunctionBoxCollection(positions)
     for ii in range(N_CONNECTIONS):
-        thisPair = pairDistances[ii]
-        firstId = thisPair[0]
-        secondId = thisPair[1]
-        firstIdCircuit = circuitById[firstId]
-        secondIdCircuit = circuitById[secondId]
-        if firstIdCircuit == secondIdCircuit:
-            continue
-        for thisId in idsByCircuit[secondIdCircuit]:
-            circuitById[thisId] = firstIdCircuit
-        idsByCircuit[firstIdCircuit] += idsByCircuit[secondIdCircuit]
-        idsByCircuit[secondIdCircuit] = []
-    
-    # print(circuitById)
-    circuitSizes = {}
-    for circuit in circuitById:
-        if circuit in circuitSizes:
-            circuitSizes[circuit] += 1
-        else:
-            circuitSizes[circuit] = 1
-    circuitSizes = list(circuitSizes.items())
-    circuitSizes.sort(key=lambda x: x[1], reverse=True)
-    circuitSizes = [x[1] for x in circuitSizes]
+        junctionBoxes.mergeNextCircuitPair()
+    circuitSizes = junctionBoxes.calculateCircuitSizes()
 
+    circuitSizes.sort(reverse=True)
     result = circuitSizes[0]
     for ii in range(1, 3):
         result *= circuitSizes[ii]
-    print("Product of three largest circuit sizes = %i" % result)
-    
+    tEnd = time.time()
+
+    print("Part 1 elapsed time = %.6fs" % (tEnd - tStart))
+    print("Product of three largest circuit sizes = %i\n" % result)
+
+    # Part 2
+    tStart = time.time()
+    nConnectionsMade = N_CONNECTIONS
+    while junctionBoxes.countCircuits() > 1:
+        junctionBoxes.mergeNextCircuitPair()
+        nConnectionsMade += 1
+    lastPairIndex = junctionBoxes.mergePairIndex - 1
+    lastPairIds = junctionBoxes.pairs[lastPairIndex][:2]
+
+    xCoordinates = []
+    for thisId in lastPairIds:
+        xCoordinates.append(junctionBoxes.positions[thisId][0])
+    tEnd = time.time()
+
+    print("Part 2 elapsed time = %.6fs" % (tEnd - tStart))
+    print("Number of connections made = %i" % nConnectionsMade)
+    print("Product of X Coordinates = %i" % math.prod(xCoordinates))
 
 
 main()
